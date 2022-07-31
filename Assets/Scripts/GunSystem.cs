12f7e3cba1e5dc1dd74c2de919a6428203353e67
Hasan Erdem AK <70165677+hasanerdemak@users.noreply.github.com>
2022-07-31 19:47:41 +0300

    Orta seviye sahne V2

--- Assets/Scripts/GunSystem.cs
+++ Assets/Scripts/GunSystem.cs
@@ -79,7 +79,7 @@ public class GunSystem : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading) Reload();
 
         //Shoot
-        if (readyToShoot && shooting && !reloading && bulletsLeft > 0 && !gunAim.GetIsOutOfBounds())
+        if (readyToShoot && shooting && !reloading && bulletsLeft > 0)// && !gunAim.GetIsOutOfBounds())
         {
             bulletsShot = bulletsPerTap;
             Shoot();
